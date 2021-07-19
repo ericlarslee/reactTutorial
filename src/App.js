8003602be49448e35d372c0d005c7e24dd7332e3
Eric Lars Lee <ericlarslee@gmail.com>
2021-07-19 17:25:48 -0500

    completed tutorial

--- src/App.js
+++ src/App.js
@@ -1,22 +1,55 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Container, Row, Col, Button, Alert, Breadcrumb, Card, Form } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Container fluid>
+          <Form>
+            <Row>
+              <Col md>
+                <Form.Group controlId="formEmail">
+                  <Form.Label>Email Address:</Form.Label>
+                  <Form.Control type="email" placeholder="Email@Email.com" />
+                  <Form.Text className="text-muted">
+                    We'll never share your email address ;)
+                  </Form.Text>
+                </Form.Group>
+              </Col>
+              <Col md>
+                <Form.Group controlId="formPassword">
+                  <Form.Label>Password:</Form.Label>
+                  <Form.Control className="mb-3" type="password" placeholder="We won't share this either" />
+                </Form.Group>
+              </Col>
+            </Row>
+            <Button variant="primary" type="submit">Log On The</Button>
+          </Form>
+          
+          <Card className="mb-3" style={{ color: '#981' }}>
+            <Card.Img src="https://picsum.photos/200/50" />
+            <Card.Body>
+              <Card.Title>
+                Card Example
+              </Card.Title>
+              <Card.Text>
+                This is an example of react bootstrap cards
+              </Card.Text>
+              <Button variant="primary">Read More</Button>
+            </Card.Body>
+          </Card>
+          <Breadcrumb>
+            <Breadcrumb.Item>Test 1</Breadcrumb.Item>
+            <Breadcrumb.Item active>Test 2</Breadcrumb.Item>
+            <Breadcrumb.Item>Test 3</Breadcrumb.Item>
+          </Breadcrumb>
+          <Alert variant="primary">This is a button</Alert>
+          <Button>Test Button</Button>
+        </Container>
       </header>
     </div>
   );
